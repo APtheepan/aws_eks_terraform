4158db05ebef4ccaf27af6d6d23e12adb5913d22
APtheepan <APtheepan@example.com>
2024-09-16 11:27:24 -0400

    first commit

--- eks/backend.tf
+++ eks/backend.tf
@@ -9,14 +9,14 @@ terraform {
     bucket = "eks-remote-bucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    profile = "Jenkins_server"
+    #profile = "Jenkins_server"
   }
  
 }
 provider "aws" {
   region = "us-east-1"
   #shared_credentials_files = ["~/.aws/credentials"]
-  profile = "Jenkins_server"
+  #profile = "Jenkins_server"
 }
 
 
